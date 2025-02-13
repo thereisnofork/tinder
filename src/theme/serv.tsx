440c6f2c421119f0297c18d3fb42bcacb9aac5bc
Mohamad Ahmadi <you@example.com>
2025-02-14 00:08:52 +0330

    remove mui

--- src/theme/serv.tsx
+++
@@ -1,7 +0,0 @@
-"use server";
-
-// import { prefixer } from "stylis";
-// import stylisRTLPlugin from "stylis-plugin-rtl";
-// import rtlPlugin from "stylis-plugin-rtl";
-
-// export { prefixer, stylisRTLPlugin, rtlPlugin };
