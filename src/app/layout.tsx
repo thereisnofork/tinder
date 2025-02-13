cc0e6006a9a95d3cf368a773e7b4aa6f37cf15ad
Mohamad Ahmadi <you@example.com>
2025-02-13 21:46:59 +0330

    init2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { RtlThemeProvider } from "../theme/mainTheme";
-import { CssBaseline } from "@mui/material";
+// import { CssBaseline } from "@mui/material";
 import Script from "next/script";
 
 const geistSans = Geist({
